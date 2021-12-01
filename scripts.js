560a8d88da2b28d97af4987b51f54a260b6c21cc
AlirezaAK2000 <a.akhoundi79@gmail.com>
2021-12-01 15:23:55 +0330

    input validation added

--- scripts.js
+++ scripts.js
@@ -1,32 +1,45 @@
+function check_input(name) {
+    var regex = /^[a-z A-Z]+$/;
+    return regex.test(name);
+}
+
 function submit_handler(e) {
     var form = document.getElementById('main-form');
     var gender_status = document.querySelector('#gender-status');
     var gender_percentage = document.querySelector('#gender-percentage');
-    var gender_status_saved = document.querySelector("#gender-status-saved")
+    var gender_status_saved = document.querySelector('#gender-status-saved');
 
     var data = new FormData(form);
     var items = {};
     for (const entry of data) {
         items[entry[0]] = entry[1];
     }
+    console.log(check_input(items['name']))
+    if (check_input(items['name'])) {
+        
+        var xmlHttp;
+        
+        xmlHttp = new XMLHttpRequest();
+        
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                var respData = JSON.parse(xmlHttp.responseText) || {};
 
-
-    var xmlHttp;
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            var respData = JSON.parse(xmlHttp.responseText) || {};
-
-            gender_status.innerText = respData['gender'];
-            gender_percentage.innerText = respData['probability'];
-        }
-    };
-    xmlHttp.open(
-        'GET',
-        'https://api.genderize.io/?name=' + items['name'],
-        true
-    );
-    xmlHttp.send(null);
+                gender_status.innerText = respData['gender'];
+                gender_percentage.innerText = respData['probability'];
+            }
+        };
+        
+        xmlHttp.open(
+            'GET',
+            'https://api.genderize.io/?name=' + items['name'],
+            true
+        );
+        
+        xmlHttp.send(null);
+    } else {
+        alert(items['name'] + " is not a valid name!!!")
+    }
 
     e.preventDefault();
 }
