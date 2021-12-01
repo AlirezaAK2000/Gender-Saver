656f9c0abeb01b3d604cd1c0a9752d7fdf17bd48
AlirezaAK2000 <a.akhoundi79@gmail.com>
2021-12-01 14:06:06 +0330

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function submit_handler(e) {
+    var elements = document.getElementById("main-form").elements;
+    var obj ={};
+    for(var i = 0 ; i < elements.length ; i++){
+        var item = elements.item(i);
+        obj[item.name] = item.value;
+    }
+    var log = document.querySelector("#gender-status");
+    log.innerText = 'hello';
+    e.preventDefault();
+
+}
+
+
+
+document.getElementById("main-form").addEventListener("submit", submit_handler ,false);
