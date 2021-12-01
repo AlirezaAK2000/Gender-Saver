417e0d0fee0640807589036796e6354aaf7e9615
AlirezaAK2000 <a.akhoundi79@gmail.com>
2021-12-01 15:04:50 +0330

    request for gender added

--- scripts.js
+++ scripts.js
@@ -1,15 +1,36 @@
 function submit_handler(e) {
-    var elements = document.getElementById("main-form").elements;
-    var obj ={};
-    for(var i = 0 ; i < elements.length ; i++){
-        var item = elements.item(i);
-        obj[item.name] = item.value;
+    var form = document.getElementById('main-form');
+    var gender_status = document.querySelector('#gender-status');
+    var gender_percentage = document.querySelector('#gender-percentage');
+    var gender_status_saved = document.querySelector("#gender-status-saved")
+
+    var data = new FormData(form);
+    var items = {};
+    for (const entry of data) {
+        items[entry[0]] = entry[1];
     }
-    var log = document.querySelector("#gender-status");
-    log.innerText = 'hello';
+
+
+    var xmlHttp;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            var respData = JSON.parse(xmlHttp.responseText) || {};
+
+            gender_status.innerText = respData['gender'];
+            gender_percentage.innerText = respData['probability'];
+        }
+    };
+    xmlHttp.open(
+        'GET',
+        'https://api.genderize.io/?name=' + items['name'],
+        true
+    );
+    xmlHttp.send(null);
+
     e.preventDefault();
 }
 
-
-
-document.getElementById("main-form").addEventListener("submit", submit_handler ,false);
+document
+    .getElementById('main-form')
+    .addEventListener('submit', submit_handler, false);
